fe8b7df562a05c9c84598b1a73503519a508c037
sheaffej <sheaffej@gmail.com>
2022-10-29 17:14:55 -0700

    convert urdf wheels to joints

--- gazebo/run/downstairs.sh
+++ gazebo/run/downstairs.sh
@@ -5,5 +5,7 @@ GZ_DIR="${MYDIR}/.."
 
 export IGN_GAZEBO_RESOURCE_PATH="${MYDIR}/../models"
 
+xacro ${MYDIR}/../../config/urdf/r2b2.xacro > ${MYDIR}/../../config/urdf/r2b2.urdf
+
 ign gazebo --render-engine ogre --gui-config ${GZ_DIR}/config/gui.config ${GZ_DIR}/worlds/downstairs.sdf
 
