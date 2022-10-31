073c54a024975423a3c07faa4cf4433bb5110266
sheaffej <sheaffej@gmail.com>
2022-10-30 20:51:13 -0700

    update xacro to work with gazebo

--- gazebo/run/downstairs.sh
+++ gazebo/run/downstairs.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+WORLD_SDF="downstairs.sdf"
+# WORLD_SDF="downstairs.processed.sdf"
+
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 GZ_DIR="${MYDIR}/.."
 
@@ -9,5 +12,5 @@ export GZ_SIM_RESOURCE_PATH="${MYDIR}/../models"
 xacro ${MYDIR}/../../config/urdf/r2b2.xacro > ${MYDIR}/../../config/urdf/r2b2.urdf
 
 # ign gazebo --render-engine ogre --gui-config ${GZ_DIR}/config/gui.config ${GZ_DIR}/worlds/downstairs.sdf
-gz sim --render-engine ogre --gui-config ${GZ_DIR}/config/gui.config ${GZ_DIR}/worlds/downstairs.sdf
+gz sim --render-engine ogre --gui-config ${GZ_DIR}/config/gui.config ${GZ_DIR}/worlds/${WORLD_SDF}
 
