412147d7a1a78b7fce72b104b9462086ccedae59
sheaffej <sheaffej@gmail.com>
2022-11-06 17:26:05 -0800

    add rviz and rqt to sim.launch.py

--- launch/r2b2.launch.py
+++ launch/r2b2.launch.py
@@ -1,5 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, LogInfo
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, LogInfo, GroupAction
 from launch.conditions import LaunchConfigurationEquals
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, Command
@@ -33,24 +33,86 @@ def generate_launch_description():
     actions.append(
         DeclareLaunchArgument('test_mode', default_value='false')
     )
+    actions.append(
+        DeclareLaunchArgument('sim_mode', default_value='false')
+    )
 
     actions.append(LogInfo(msg=['Arg: robot_model_file = ', LaunchConfiguration('robot_model_file')]))
     actions.append(LogInfo(msg=['Arg: run_nav = ', LaunchConfiguration('run_nav')]))
     actions.append(LogInfo(msg=['Arg: nav_params_file = ', LaunchConfiguration('nav_params_file')]))
     actions.append(LogInfo(msg=['Arg: map_file = ', LaunchConfiguration('map_file')]))
     actions.append(LogInfo(msg=['Arg: test_mode = ', LaunchConfiguration('test_mode')]))
+    actions.append(LogInfo(msg=['Arg: sim_mode = ', LaunchConfiguration('sim_mode')]))
+
+    actions.append(
+        # Hardware robot nodes. Not used during simulations
+        GroupAction(
+            condition=LaunchConfigurationEquals('sim_mode', 'false'),
+            actions=[
+                Node(
+                    name='roboclaw',
+                    package='roboclaw_driver2',
+                    executable='roboclaw_node',
+                    parameters=[{
+                        'loop_hz': 20,
+                        'speed_cmd_topic': 'roboclaw/speed_command',
+                        'stats_topic': 'roboclaw/stats',
+                        'test_mode': LaunchConfiguration('test_mode'),
+                        'dev_names': '/dev/ttyACM0,/dev/ttyACM1'
+                    }]
+                ),
+
+                Node(
+                    name='base_node',
+                    package='r2b2_base',
+                    executable='base_node',
+                    parameters=[{
+                        'loop_hz': 20,
+                        'cmd_vel_topic': 'cmd_vel',
+                        'odom_topic': 'odom',
+                        'speed_command_topic': 'roboclaw/speed_command',
+                        'roboclaw_front_stats_topic': 'roboclaw/stats',
+                        'log_level': 'info',
+                        'publish_odom_tf': True
+                    }]
+                ),
+
+                Node(
+                    name='rplidar',
+                    condition=LaunchConfigurationEquals('test_mode', 'false'),
+                    package='rplidar_ros',
+                    executable='rplidar_composition',
+                    parameters=[{
+                        'serial_port': '/dev/ttyUSB0',
+                        'serial_baudrate': 115200,
+                        'frame_id': 'scanner_link',
+                        'inverted': False,
+                        'angle_compensate': True
+                    }]
+                ),
+
+                IncludeLaunchDescription(
+                    condition=LaunchConfigurationEquals('test_mode', 'false'),
+                    launch_description_source=PythonLaunchDescriptionSource([
+                        PathJoinSubstitution([FindPackageShare('realsense2_camera'), 'launch/rs_launch.py'])
+                    ]),
+                    launch_arguments={
+                        'test_mode': 'True'
+                    }.items()
+                ),
+            ]
+        )
+    )
 
     actions.append(
         Node(
-            name='roboclaw',
-            package='roboclaw_driver2',
-            executable='roboclaw_node',
+            name='robot_state_publisher',
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
             parameters=[{
-                'loop_hz': 20,
-                'speed_cmd_topic': 'roboclaw/speed_command',
-                'stats_topic': 'roboclaw/stats',
-                'test_mode': LaunchConfiguration('test_mode'),
-                'dev_names': '/dev/ttyACM0,/dev/ttyACM1'
+                'robot_description': ParameterValue(
+                    Command(['xacro', ' ', LaunchConfiguration('robot_model_file')]),
+                    value_type=str)
             }]
         )
     )
@@ -63,50 +125,6 @@ def generate_launch_description():
             parameters=[{
                 'use_gui': False
             }]
-        )
-    )
-
-    actions.append(
-        Node(
-            name='robot_state_publisher',
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            parameters=[{
-                'robot_description': ParameterValue(
-                    Command([
-                        'xacro',
-                        ' ',
-                        LaunchConfiguration('robot_model_file')
-                    ]), value_type=str)
-            }]
-        )
-    )
-
-    actions.append(
-        Node(
-            condition=LaunchConfigurationEquals('test_mode', 'false'),
-            name='rplidar',
-            package='rplidar_ros',
-            executable='rplidar_composition',
-            parameters=[{
-                'serial_port': '/dev/ttyUSB0',
-                'serial_baudrate': 115200,
-                'frame_id': 'scanner_link',
-                'inverted': False,
-                'angle_compensate': True
-            }]
-        )
-    )
-
-    actions.append(
-        IncludeLaunchDescription(
-            condition=LaunchConfigurationEquals('test_mode', 'false'),
-            launch_description_source=PythonLaunchDescriptionSource([
-                PathJoinSubstitution([FindPackageShare('realsense2_camera'), 'launch/rs_launch.py'])
-            ]),
-            launch_arguments={
-                'test_mode': 'True'
-            }.items()
         )
     )
 
--- launch/sim.launch.py
+++ launch/sim.launch.py
@@ -1,7 +1,10 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
-from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.conditions import LaunchConfigurationEquals
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -17,8 +20,31 @@ def generate_launch_description():
         DeclareLaunchArgument('gui_config', default_value='/ros2/src/r2b2/gazebo/config/gui.config')
     )
     actions.append(
+        DeclareLaunchArgument('run_ros_nodes', default_value='true')
+    )
+    actions.append(
         DeclareLaunchArgument('gz_bridge_config_file',
             default_value='/ros2/src/r2b2/gazebo/config/gz_bridge.params.yaml'
+        )
+    )
+
+    # Args from r2b2.launch.py
+    actions.append(
+        DeclareLaunchArgument('robot_model_file',
+            default_value=PathJoinSubstitution([FindPackageShare('r2b2'), 'config/urdf/r2b2.xacro'])
+        )
+    )
+    actions.append(
+        DeclareLaunchArgument('run_nav', default_value='true')
+    )
+    actions.append(
+        DeclareLaunchArgument('nav_params_file',
+            default_value=PathJoinSubstitution([FindPackageShare('r2b2'), 'config/params/nav2_params.yaml'])
+        )
+    )
+    actions.append(
+        DeclareLaunchArgument('map_file',
+            default_value=PathJoinSubstitution([FindPackageShare('r2b2'), 'config/map/b2-downstairs4.yaml'])
         )
     )
 
@@ -41,6 +67,31 @@ def generate_launch_description():
             parameters=[{
                 'config_file': LaunchConfiguration('gz_bridge_config_file')
             }]
+        )
+    )
+
+    actions.append(
+        IncludeLaunchDescription(
+            condition=LaunchConfigurationEquals('run_ros_nodes', 'true'),
+            launch_description_source=PathJoinSubstitution(
+                [FindPackageShare('r2b2'), 'launch', 'r2b2.launch.py']
+            ),
+            launch_arguments=[
+                ('sim_mode', 'true'),
+                ('robot_model_file', LaunchConfiguration('robot_model_file')),
+                ('run_nav', LaunchConfiguration('run_nav')),
+                ('nav_params_file', LaunchConfiguration('nav_params_file')),
+                ('map_file', LaunchConfiguration('map_file'))
+            ]
+        )
+    )
+
+    actions.append(
+        IncludeLaunchDescription(
+            condition=LaunchConfigurationEquals('run_ros_nodes', 'true'),
+            launch_description_source=PathJoinSubstitution(
+                [FindPackageShare('r2b2'), 'launch', 'display.launch.yaml']
+            )
         )
     )
 
