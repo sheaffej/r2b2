64dfa4f166216859464a355c8160c428d1ef450b
sheaffej <sheaffej@gmail.com>
2022-12-01 21:24:33 -0800

    move scripts

--- scripts/docker/build_docker.sh
+++ scripts/docker/build_docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-REPO_DIR="/home/b2/r2b2_project/r2b2"
+REPO_DIR="${HOME}/r2b2_project/r2b2"
 
 cd ${REPO_DIR}
 docker build -t r2b2-robot .
---
+++ scripts/gazebo/spawn_robot.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# GZ_DIR="${MYDIR}/.."
+# export IGN_GAZEBO_RESOURCE_PATH="${MYDIR}/../models"
+
+xacro ${MYDIR}/../../config/urdf/r2b2.xacro > /tmp/r2b2.urdf
+
+
+# ign service -s /world/downstairs/remove \
+# --reqtype ignition.msgs.Entity --reptype ignition.msgs.Boolean --timeout 1000 \
+# --req 'id: 163'
+
+# ign service -s /world/downstairs/create \
+gz service -s /world/downstairs/create \
+--reqtype gz.msgs.EntityFactory --reptype gz.msgs.Boolean --timeout 1000 \
+--req 'sdf_filename: "/tmp/r2b2.urdf", name: "r2b2_model", pose: {position: {x: 2 y: -1}}'
